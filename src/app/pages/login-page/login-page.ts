fd4c9fa26dfe927d5ebb7e8b8fad2552bdd2f9b6
mspolermo <mspolermo@gmail.com>
2026-01-29 18:46:43 +0500

    form in process

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
+  form = new FormGroup({
+    username: new FormControl(null),
+    password: new FormControl(null)
+  })
 
+  onSubmit() {
+    console.log(this.form.value)
+  }
 }
