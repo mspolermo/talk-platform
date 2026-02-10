f8159e22a15acf53e9df8f887ca453e9bb7d7790
mspolermo <mspolermo@gmail.com>
2026-02-10 18:06:16 +0500

    sidebar - menu seems completed

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { SvgIcon } from '../svg-icon/svg-icon';
+import { SvgIcon } from '../svg-icon/svg-icon'
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIcon],
+  imports: [SvgIcon, NgForOf],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
-
+  menuItems = [
+    { label: 'Моя страница', icon: 'home', link: ''},
+    { label: 'Чаты', icon: 'chats', link: 'chats'},
+    { label: 'Поиск', icon: 'search', link: 'search'}
+  ]
 }
