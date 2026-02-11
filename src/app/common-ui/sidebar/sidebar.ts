62b05fdd4d90d3e4e8c766bb460164aea06f5acc
mspolermo <mspolermo@gmail.com>
2026-02-11 19:07:02 +0500

    subscriber-card started

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SvgIcon } from '../svg-icon/svg-icon'
 import { NgForOf } from '@angular/common';
+import { SubscriberCard } from "./subscriber-card/subscriber-card";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIcon, NgForOf],
+  imports: [SvgIcon, NgForOf, SubscriberCard],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
