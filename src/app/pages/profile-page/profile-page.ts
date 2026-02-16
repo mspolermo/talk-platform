8cc6461545523e9613bd9a4a808ef249e390d830
mspolermo <mspolermo@gmail.com>
2026-02-16 18:53:49 +0500

    dynamic profile page started

--- src/app/pages/profile-page/profile-page.ts
+++ src/app/pages/profile-page/profile-page.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProfileHeader } from "../../common-ui/profile-header/profile-header";
+import { ProfileService } from '../../data/services/profile';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-profile-page',
@@ -8,5 +11,10 @@ import { ProfileHeader } from "../../common-ui/profile-header/profile-header";
   styleUrl: './profile-page.scss',
 })
 export class ProfilePage {
+  profileService = inject(ProfileService)
+  route = inject(ActivatedRoute)
 
+  profile$ = this.route.params.pipe(
+    switchMap()
+  )
 }
