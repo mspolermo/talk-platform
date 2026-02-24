4ce11eaacbcfb393f8338abb5e4e55ef6eddf03c
mspolermo <mspolermo@gmail.com>
2026-02-24 20:11:53 +0500

    settings-page started

---
+++ src/app/pages/settings-page/settings-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProfileHeader } from "../../common-ui/profile-header/profile-header";
+
+@Component({
+  selector: 'app-settings-page',
+  imports: [ProfileHeader],
+  templateUrl: './settings-page.html',
+  styleUrl: './settings-page.scss',
+})
+export class SettingsPage {
+
+}
