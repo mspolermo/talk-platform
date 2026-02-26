863f4332c8477f8d3adf71fbe63d349fae59b110
mspolermo <mspolermo@gmail.com>
2026-02-26 19:56:49 +0500

    settings form in process

--- src/app/pages/settings-page/settings-page.ts
+++ src/app/pages/settings-page/settings-page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProfileHeader } from "../../common-ui/profile-header/profile-header";
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings-page',
-  imports: [ProfileHeader],
+  imports: [ProfileHeader, ReactiveFormsModule],
   templateUrl: './settings-page.html',
   styleUrl: './settings-page.scss',
 })
 export class SettingsPage {
+  fb = inject(FormBuilder)
 
+  form = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    username: ['', Validators.required],
+    description: [''],
+    stack: ['']
+  })
 }
