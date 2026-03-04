8b2936f043b508f459d7418f46541b5603da268c
mspolermo <mspolermo@gmail.com>
2026-03-04 19:25:02 +0500

    drag-and-drop dirictive added

--- src/app/pages/settings-page/avatar-upload/avatar-upload.ts
+++ src/app/pages/settings-page/avatar-upload/avatar-upload.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SvgIcon } from "../../../common-ui/svg-icon/svg-icon";
+import { Dnd } from '../../../common-ui/directives/dnd';
 
 @Component({
   selector: 'app-avatar-upload',
-  imports: [SvgIcon],
+  imports: [SvgIcon, Dnd],
   templateUrl: './avatar-upload.html',
   styleUrl: './avatar-upload.scss',
 })
 export class AvatarUpload {
+
+  preview = signal<string>("/assets/imgs/avatar-placeholder.png")
+
   fileBrowserHandler(event: Event) {
-    const files = (event.target as HTMLInputElement)?.files as FileList
+    const file = (event.target as HTMLInputElement)?.files?.[0]
+
+    this.processFile(file)
+  }
+
+  onFileDropped(file: File) {
+    this.processFile(file)
+  }
+
+  processFile(file?: File | null) {
+    if (!file || !file.type.match('image')) return
+
+    const reader = new FileReader()
+
+    reader.onload = event => {
+      this.preview.set(event.target?.result?.toString() ?? '')
+    }
+
+    reader.readAsDataURL(file)
   }
 }
